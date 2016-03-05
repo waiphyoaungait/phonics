7cf5d27e434f0413a6aa93bf2487d18347942727
Armin Naimi <armin.naimi@gmail.com>
2016-03-05 23:24:05 +0200

    Gotta fix an issue where all audio files are playing when one letter is clicked

--- js/grid.js
+++ js/grid.js
@@ -1,14 +1,19 @@
 $(document).ready( function () {
     $.getJSON('../data/letters.json', function(data) {
         $.each(data.letters, function(letter, values) {
-            $('.container').append('<div class="col-md-3 col-lg-2 col-xs-6 letter-item">' + letter + '</div>' + '<audio class="js-audio"><source src="audio/'+values.audio+'.mp3"></source></audio>');
+            $('.container').append('<div class="col-md-3 col-lg-2 col-xs-6 letter-item">' + letter + '</div>' + '<audio class="js-sound"><source src="audio/'+values.audio+'.mp3"></source></audio>');
         });
     });
-    
-    var audio = $(".js-audio")[0];
-    
+
     $('.container').on( 'click', '.letter-item', function () {
-        alert('playing' + audio)
-        audio.play();
+        $(".js-sound").trigger('play');
+    });
+
+    $("audio").on("play", function(){
+        var _this = $(this);
+        $("audio").each(function(i,el){
+            if(!$(el).is(_this))
+                $(el).get(0).pause();
+        });
     });
 });
